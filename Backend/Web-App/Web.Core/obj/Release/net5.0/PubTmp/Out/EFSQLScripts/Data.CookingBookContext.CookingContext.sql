aaca2b9951d15471fbeeccf0108a6307cdbc5ae2
Manuel Peise <manuel.p80@gmx.de>
2022-01-09 21:52:50 +0100

    rebuild

---
+++ Backend/Web-App/Web.Core/obj/Release/net5.0/PubTmp/Out/EFSQLScripts/Data.CookingBookContext.CookingContext.sql
@@ -0,0 +1,50 @@
+﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) NOT NULL,
+    `ProductVersion` varchar(32) NOT NULL,
+    PRIMARY KEY (`MigrationId`)
+);
+
+START TRANSACTION;
+
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20211222215949_initDb')
+BEGIN
+    CREATE TABLE `Ingredients` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `Name` text NOT NULL,
+        `Type` int NOT NULL,
+        PRIMARY KEY (`Id`)
+    );
+END;
+
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20211222215949_initDb')
+BEGIN
+    CREATE TABLE `MenuIngredients` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `MenuId` int NOT NULL,
+        `IngredientId` int NOT NULL,
+        `Amount` int NOT NULL,
+        `Unit` int NOT NULL,
+        PRIMARY KEY (`Id`)
+    );
+END;
+
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20211222215949_initDb')
+BEGIN
+    CREATE TABLE `Menus` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `Name` text NOT NULL,
+        `Type` int NOT NULL,
+        `Description` text NOT NULL,
+        `HowTo` text NULL,
+        PRIMARY KEY (`Id`)
+    );
+END;
+
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20211222215949_initDb')
+BEGIN
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20211222215949_initDb', '5.0.13');
+END;
+
+COMMIT;
+
